efb58b9a4df77a5415df36392d01c49a3335bbca
PrajwalFewaliRana <prajwalmagar68@gmail.com>
2026-01-25 23:20:43 +0545

    add Properties from django home bnb

--- djangobnb_backend/property/api.py
+++ djangobnb_backend/property/api.py
@@ -3,10 +3,14 @@ from rest_framework.decorators import api_view,authentication_classes,permission
 
 from .models import Property
 from .serializers import PropertiesListSerializer
+from .forms import PropertyForm
+
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import AllowAny
 
 @api_view(['GET'])
 @authentication_classes([])
-@permission_classes([])
+@permission_classes([AllowAny])
 def properties_list(request):
     properties = Property.objects.all()
     serializer= PropertiesListSerializer(properties,many=True)
@@ -14,3 +18,19 @@ def properties_list(request):
     return JsonResponse({
         'data':serializer.data
     })
+    
+
+@api_view(['POST','FILES'])
+@permission_classes([IsAuthenticated]) #must be logged in
+def create_property(request):
+    form = PropertyForm(request.POST,request.FILES)
+    
+    if form.is_valid():
+        property = form.save(commit=False)
+        property.landlord = request.user
+        property.save()
+        
+        return JsonResponse({'success':True})
+    else:
+        print('error',form.errors,form.non_field_errors)
+        return JsonResponse({'errors':form.errors.as_json()},status =400)
