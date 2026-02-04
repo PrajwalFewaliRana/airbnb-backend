caf47128b66f9cab3a6d79c79e742cbadf35a34a
PrajwalFewaliRana <prajwalmagar68@gmail.com>
2026-02-05 00:03:55 +0545

    hellp

--- djangobnb_backend/useraccount/api.py
+++ djangobnb_backend/useraccount/api.py
@@ -1,6 +1,10 @@
 from .serializers import UserDetailSerializer
 from django.http import JsonResponse
 from rest_framework.decorators import api_view,authentication_classes,permission_classes
+from property.serializers import ReservationsListSerializer
+from rest_framework.permissions import IsAuthenticated
+
+
 
 from .models import User
 
@@ -12,4 +16,11 @@ def landlord_detail(request,pk):
     user = User.objects.get(pk=pk)
     serializer = UserDetailSerializer(user,many=False)
     
+    return JsonResponse(serializer.data,safe=False)
+
+
+@api_view(['GET'])
+def reservations_list(request):
+    reservations = request.user.reservations.all()
+    serializer= ReservationsListSerializer(reservations,many=True)
     return JsonResponse(serializer.data,safe=False)
